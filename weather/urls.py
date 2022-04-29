f13c105fb1f4c9a98d7e9248b9b5423372525a4e
dmit9 <devyatin5@gmail.com>
2022-04-29 13:34:49 +0300

    Lesson end

---
+++ weather/urls.py
@@ -0,0 +1,7 @@
+﻿from django import views
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index),
+]
---
+++ weather/views.py
@@ -0,0 +1,31 @@
+from multiprocessing import context
+from django.shortcuts import render
+import requests
+from .models import City
+from .forms import CityForm
+
+def index(request):
+    apid = '4e6e32c70272e312c474cbaef0352273'
+    url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' + apid
+
+    if (request.method == 'POST'):
+        form = CityForm(request.POST)
+        form.save()
+
+    form = CityForm()
+
+    cities = City.objects.all()
+
+    all_cities = []
+
+    for city in cities:
+        res = requests.get(url.format(city.name)).json()
+        city_info = {
+            'city' : city.name,
+            'temp' : res['main']["temp"],
+            'icon' : res['weather'][0]["icon"]
+        }
+        all_cities.append(city_info)
+
+    context = {'all_info': all_cities, 'form': form}
+    return render(request, 'weather/index.html', context)
